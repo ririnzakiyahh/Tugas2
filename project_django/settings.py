605c579b3aadbbef459e993edb754746f84354a1
ririnzakiyah02 <ririnzakiyah02@Gmail.com>
2022-09-13 14:26:33 +0700

    commitkedua

--- project_django/settings.py
+++ project_django/settings.py
@@ -141,3 +141,5 @@ for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
