0172eaab677769585c07119bfe1d8d061a78bc1f
ririnzakiyah02 <ririnzakiyah02@Gmail.com>
2022-09-21 21:58:28 +0700

    commit kedua

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -37,8 +37,8 @@ def show_json(request):
     data = MyWatchlistItem.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
-def show_json_by_id(request, id):
-    data = MyWatchlistItem.objects.filter(pk=id)
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+# def show_json_by_id(request, id):
+#     data = MyWatchlistItem.objects.filter(pk=id)
+#     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 
